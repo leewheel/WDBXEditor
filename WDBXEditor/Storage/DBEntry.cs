35cc6f9d8628084215f1a88ad0840429a4674bd2
robinsch <hello@robinsch.com>
2024-03-09 13:13:35 +0100

    Export to SQL as snake case with _dbc suffix

--- WDBXEditor/Storage/DBEntry.cs
+++ WDBXEditor/Storage/DBEntry.cs
@@ -23,6 +23,76 @@ using System.IO.MemoryMappedFiles;
 using System.Security.AccessControl;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Globalization;
+using Extensions;
+
+namespace Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToSnakeCase(this string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+
+            var builder = new StringBuilder(text.Length + Math.Min(2, text.Length / 5));
+            var previousCategory = default(UnicodeCategory?);
+
+            for (var currentIndex = 0; currentIndex < text.Length; currentIndex++)
+            {
+                var currentChar = text[currentIndex];
+                if (currentChar == '_')
+                {
+                    builder.Append('_');
+                    previousCategory = null;
+                    continue;
+                }
+
+                var currentCategory = char.GetUnicodeCategory(currentChar);
+                switch (currentCategory)
+                {
+                    case UnicodeCategory.UppercaseLetter:
+                    case UnicodeCategory.TitlecaseLetter:
+                        if (previousCategory == UnicodeCategory.SpaceSeparator ||
+                            previousCategory == UnicodeCategory.LowercaseLetter ||
+                            previousCategory != UnicodeCategory.DecimalDigitNumber &&
+                            previousCategory != null &&
+                            currentIndex > 0 &&
+                            currentIndex + 1 < text.Length &&
+                            char.IsLower(text[currentIndex + 1]))
+                        {
+                            builder.Append('_');
+                        }
+
+                        currentChar = char.ToLower(currentChar, CultureInfo.InvariantCulture);
+                        break;
+
+                    case UnicodeCategory.LowercaseLetter:
+                    case UnicodeCategory.DecimalDigitNumber:
+                        if (previousCategory == UnicodeCategory.SpaceSeparator)
+                        {
+                            builder.Append('_');
+                        }
+                        break;
+
+                    default:
+                        if (previousCategory != null)
+                        {
+                            previousCategory = UnicodeCategory.SpaceSeparator;
+                        }
+                        continue;
+                }
+
+                builder.Append(currentChar);
+                previousCategory = currentCategory;
+            }
+
+            return builder.ToString();
+        }
+    }
+}
 
 namespace WDBXEditor.Storage
 {
@@ -489,7 +559,7 @@ namespace WDBXEditor.Storage
 			if (buildNumber == (int)WDBXEditor.Common.Constants.ExpansionFinalBuild.Ascension)
 				buildNumber = (int)WDBXEditor.Common.Constants.ExpansionFinalBuild.WotLK;
 
-			string tableName = $"db_{TableStructure.Name}_{buildNumber}";
+			string tableName = $"{TableStructure.Name.ToSnakeCase()}_dbc";
 
 			StringBuilder sb = new StringBuilder();
 			sb.AppendLine($"DROP TABLE IF EXISTS `{tableName}`; ");
